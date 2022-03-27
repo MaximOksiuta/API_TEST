9ea5121285b7023f8ed5e23e94c2d88f006e116a
MaximOksiuta <63787095+MaximOksiuta@users.noreply.github.com>
2022-03-27 19:32:21 +0300

    debug (rename method post user)

--- API_TEST/urls.py
+++ API_TEST/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from API_TEST.views import UserCreateView, GetUsersView, GetUserDetailsView
 
 urlpatterns = [
-    path("user/create", UserCreateView.as_view()),
+    path("user", UserCreateView.as_view()),
     path("users/get_all", GetUsersView.as_view()),
     path("user/<int:pk>", GetUserDetailsView.as_view())
 ]
