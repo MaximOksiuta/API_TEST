3e82a6987edd1bb08e01a64acf40780dc64dd010
MaximOksiuta <63787095+MaximOksiuta@users.noreply.github.com>
2022-03-27 21:32:20 +0300

    result method change 3

--- API_TEST/serializers.py
+++ API_TEST/serializers.py
@@ -41,7 +41,7 @@ class AddressPostSerializer(serializers.ModelSerializer):
 class AddressContentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
-        fields = ('id', 'address')
+        fields = ('address')
 
 
 class DevelopStatusSerializer(serializers.ModelSerializer):
